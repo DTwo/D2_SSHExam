f74e057e691438f84da785ffa68626c34ed5e4cc
zhengxiaobin <hubert12138@gmail.com>
2015-09-16 16:10:58 +0800

    Price change I

--- src/main/java/com/hand/dao/impl/PriceConfigDaoImpl.java
+++ src/main/java/com/hand/dao/impl/PriceConfigDaoImpl.java
@@ -1,7 +1,28 @@
 package com.hand.dao.impl;
 
+import com.hand.Entity.Priceconfig;
 import com.hand.dao.PriceConfigDao;
 
 public class PriceConfigDaoImpl implements PriceConfigDao{
+
+	public void addPriceConfig(Priceconfig priceConfig) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Priceconfig getPriceConfigwithCandT(String cust_code, String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Priceconfig getPriceConfigwithId(Integer priceConfigId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void editPriceConfig(Priceconfig priceConfig) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
